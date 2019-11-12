1387e348a6bad0771c01d35a98a7df5073d04239
kuun <kuunwang@gmail.com>
2019-11-12 17:26:53 +0800

    add connect testing code

--- ui/ConnectDialog.py
+++ ui/ConnectDialog.py
@@ -1,5 +1,10 @@
+import traceback
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QPushButton, QHBoxLayout, QVBoxLayout, QFormLayout, QLineEdit, QSpinBox
+from PyQt5.QtWidgets import QDialog, QPushButton, QHBoxLayout, QVBoxLayout, QFormLayout, QLineEdit, QSpinBox, \
+    QMessageBox
+
+from trex.astf.trex_astf_client import ASTFClient
 
 
 class ConnectDialog(QDialog):
@@ -14,30 +19,31 @@ class ConnectDialog(QDialog):
         # build form
         form = QFormLayout()
         vbox.addLayout(form)
-        ip_edit = QLineEdit(self)
-        form.addRow(self.tr('TRex IP:'), ip_edit)
-        rpc_port_edit = QSpinBox()
-        rpc_port_edit.setRange(1, 65535)
-        rpc_port_edit.setValue(4501)
-        form.addRow(self.tr('RPC Port:'), rpc_port_edit)
-        zmq_port_edit = QSpinBox()
-        zmq_port_edit.setRange(1, 65535)
-        zmq_port_edit.setValue(4500)
-        form.addRow(self.tr('ZMQ Port:'), zmq_port_edit)
-        scapy_port_edit = QSpinBox()
-        scapy_port_edit.setRange(1, 65535)
-        scapy_port_edit.setValue(4507)
-        form.addRow(self.tr('Scapy Port'), scapy_port_edit)
+        self.ip_edit = QLineEdit(self)
+        form.addRow(self.tr('TRex IP:'), self.ip_edit)
+        self.rpc_port_edit = QSpinBox()
+        self.rpc_port_edit.setRange(1, 65535)
+        self.rpc_port_edit.setValue(4501)
+        form.addRow(self.tr('RPC Port:'), self.rpc_port_edit)
+        self.zmq_port_edit = QSpinBox()
+        self.zmq_port_edit.setRange(1, 65535)
+        self.zmq_port_edit.setValue(4500)
+        form.addRow(self.tr('ZMQ Port:'), self.zmq_port_edit)
+        self.scapy_port_edit = QSpinBox()
+        self.scapy_port_edit.setRange(1, 65535)
+        self.scapy_port_edit.setValue(4507)
+        form.addRow(self.tr('Scapy Port'), self.scapy_port_edit)
 
         hbox = QHBoxLayout()
         vbox.addLayout(hbox)
 
         hbox.addStretch(1)
-        connectBtn = QPushButton(self.tr('Connect'), self)
-        hbox.addWidget(connectBtn)
-        cancelBtn = QPushButton(self.tr('Cancel'), self)
-        cancelBtn.clicked.connect(self.cancel)
-        hbox.addWidget(cancelBtn)
+        connect_btn = QPushButton(self.tr('Connect'), self)
+        connect_btn.clicked.connect(self.connect_server)
+        hbox.addWidget(connect_btn)
+        cancel_btn = QPushButton(self.tr('Cancel'), self)
+        cancel_btn.clicked.connect(self.cancel)
+        hbox.addWidget(cancel_btn)
 
         self.setWindowTitle(self.tr('Connect to TRex'))
         self.setWindowModality(Qt.ApplicationModal)
@@ -47,4 +53,17 @@ class ConnectDialog(QDialog):
         self.close()
 
     def connect_server(self):
-        pass
+        server = self.ip_edit.text()
+        rpc_port = self.rpc_port_edit.value()
+        zmq_port = self.zmq_port_edit.value()
+        client = ASTFClient(server=server, sync_port=rpc_port, async_port=zmq_port)
+        try:
+            client.connect()
+            msg_box = QMessageBox()
+            msg_box.setText('Successfuly')
+            msg_box.exec()
+        except Exception as e:
+            print('failed to connect to TRex server:', traceback.format_exc())
+            msg_box = QMessageBox()
+            msg_box.setText('Failed')
+            msg_box.exec()
